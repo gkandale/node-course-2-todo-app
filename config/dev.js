e838470b22fe804b76cfeb064e1c6f79d44d2404
gkandale <gkandale@cisco.com>
2018-06-04 22:54:03 -0400

    make it better

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,6 @@ module.exports = {
         '665896090171-m1gj65ebnth73ur5vjt5jrhdfvg91bje.apps.googleusercontent.com',
     googleClientSecret: 'fndHbBB4KXqbgQYggkq7wngx',
     //mongoURI: 'mongodb://gkandale:Fungu50!@ds133776.mlab.com:33776/blog_dev',
-    mongoURI: 'mongodb://ds249530.mlab.com:49530/heroku_cckh7gqz',
+    mongoURI: 'mongodb://gkandale:Fungu50!@ds249530.mlab.com:49530/heroku_cckh7gqz',
     cookieKey: '123123123'
 };
