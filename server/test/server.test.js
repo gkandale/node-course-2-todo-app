ed39736ed88d94b1dda17dc2c4f8c3d688a0d4bb
gkandale <gkandale@cisco.com>
2018-06-04 20:12:29 -0400

    Test invalid GET /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const {app} = require('../server');
 const {todo} = require('../models/todo');
 const {user} = require('../models/user');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First Test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -17,14 +20,11 @@ const users = [{
     email: 'skandale@gmail.com'
 }];
 
-beforeEach((done) => {
-    // todo.remove({}).then(() => {
-    //     return todo.insertMany(todos);
-    // }).then(() => done());
-
-    user.remove({}).then(() => {
-        return user.insertMany(users);
-    }).then(() => done());
+beforeEach((done) => {todo.remove({})
+.then(() => {todo.insertMany(todos)})
+.then(() => {user.remove({})})
+.then(() => {user.insertMany(users)})
+.then(() => done());
 });
 
 // describe('POST /todos', () => {
@@ -81,26 +81,56 @@ beforeEach((done) => {
 //     });
 // });
 
-describe('POST /users', () => {
-    it('should create new user', (done) => {
-        var email = 'toronto.maple@leafs.com';
+// describe('POST /users', () => {
+//     it('should create new user', (done) => {
+//         var email = 'toronto.maple@leafs.com';
+//         request(app)
+//             .post('/users')
+//             .send({email})
+//             .expect(200)
+//             .expect((res) => {
+//                 expect(res.body.email).toBe(email);
+//             })
+//             .end((err, res) => {
+//                 if (err) {
+//                     return done(err);
+//                 }
+//                 user.find({email}).then((users) => {
+//                     expect(user.length).toBe(3);
+//                     //console.log('Users at [0] = ', users[0].email);
+//                     expect(users[0].email).toBe(email);
+//                     done();
+//                 }).catch((e) => done(e));
+//             });
+//     });
+//});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
         request(app)
-            .post('/users')
-            .send({email})
+            .get(`/todos/${todos[0]._id.toHexString()}`)
             .expect(200)
             .expect((res) => {
-                expect(res.body.email).toBe(email);
+                expect(res.body.todo.text).toBe(todos[0].text);
             })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-                user.find({email}).then((users) => {
-                    expect(user.length).toBe(3);
-                    //console.log('Users at [0] = ', users[0].email);
-                    expect(users[0].email).toBe(email);
-                    done();
-                }).catch((e) => done(e));
-            });
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var objectIDHex = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${objectIDHex}`)
+            .expect(404)
+            .end(done);
+
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        var rand = Math.floor(Math.random() * 1000000);
+        console.log('Random Number ', rand.toString());
+        request(app)
+            .get(`/todos/${rand.toString()}`)
+            .expect(404)
+            .end(done)
     });
 });
