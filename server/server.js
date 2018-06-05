975d5e1b0380e85407e3916f6fb4eafff0795a70
gkandale <gkandale@cisco.com>
2018-06-04 22:31:52 -0400

    make it better

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { todo } = require('./models/todo');
 const { user } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -59,8 +60,8 @@ app.get('/todos/:id', (req,res) => {
     }).catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => {
-   console.log('Started on port 3000');
+app.listen(port, () => {
+   console.log(`Started on port ${port}`);
 });
 
 module.exports = { app };
