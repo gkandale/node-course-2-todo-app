71d950d1e3ba2d09b40fe536f0edd84c9fdfa215
gkandale <gkandale@cisco.com>
2018-06-04 22:52:46 -0400

    make it better

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 
-module.exports = {
-    mongoURI: 'mongodb://gkandale:Fungu50!@ds249530.mlab.com:49530/heroku_cckh7gqz'
-}
+// module.exports = {
+//     mongoURI: 'mongodb://gkandale:Fungu50!@ds249530.mlab.com:49530/heroku_cckh7gqz'
+// }
