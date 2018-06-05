1432eae81c704669af83ae5ebb0db38ad9dfe2af
gkandale <gkandale@cisco.com>
2018-06-04 23:08:56 -0400

    make it better

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const keys = require('../../config/keys');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.MONGODB_URI);
 
 module.exports = { mongoose };
